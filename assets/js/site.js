260e9d5eebc02f05f1159902197160ec805174d8
Zhitao Gong <zhitaao.gong@gmail.com>
2013-02-27 14:57:51 -0600

    url thing

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,16 +5,16 @@
 (function ($) {
 
     menuidx = {
-        '/wdevelopment/home/': 1,
-        '/wdevelopment/document/': 2,
-        '/wdevelopment/about/': 3,
+        '/WDEvelopment/home/': 1,
+        '/WDEvelopment/document/': 2,
+        '/WDEvelopment/about/': 3,
     };
 
     $(document).ready(function() {
 
         // highlight main menu
         var path = window.location.pathname;
-        if (path != '/wdevelopment/')
+        if (path != '/WDEvelopment/')
             $('#mainnav li:nth-child(' + menuidx[path] + ')')
             .addClass('active');
 
